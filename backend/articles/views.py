8b16d90dcb710d10c77d9184e425840d6cd036e1
Rosa-bel <129285392+Rosa-bel@users.noreply.github.com>
2024-02-01 12:15:53 +0100

    added "favoris" and "filter" requests method

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -50,25 +50,21 @@ class ArticleDetailsMod(generics.RetrieveUpdateDestroyAPIView):
 class SearchResults(APIView):
     
     permission_classes = [permissions.AllowAny]
-    def get(self, request):
-        if request.method == 'GET':
-            resultat = discover_Article()
-            articles = Article.objects.filter(id__in=resultat)
-            
-            
-            serializer = Article_results(articles, many=True)
-            serialized_data = serializer.data
-            json_data = json.dumps(serialized_data)
-            print(json_data)
-            return Response(json_data, status=status.HTTP_200_OK)
-        
     def post(self, request):
         if request.method == 'POST':
         
             data = request.data
+            
+            
             query = data.get('keywords')
+            
+        
             search_results_ids = search_Article(query)
+            
+            
             articles = Article.objects.filter(id__in=search_results_ids)
+            
+            
             serializer = Article_results(articles, many=True)
             serialized_data = serializer.data
             json_data = json.dumps(serialized_data)
@@ -103,7 +99,47 @@ class SearchResults(APIView):
 
             #     response_data.append(article_data)
         
-   
+    def get(self, request):
+        if request.method == 'GET':
+            resultat = discover_Article()
+            articles = Article.objects.filter(id__in=resultat)
+            
+            
+            serializer = Article_results(articles, many=True)
+            serialized_data = serializer.data
+            json_data = json.dumps(serialized_data)
+            print(json_data)
+            return Response(json_data, status=status.HTTP_200_OK)
+        
+
+class Filter_results(APIView):
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        if request.method == 'POST':
+        
+            data = request.data
+            
+            
+            query = data.get('keywords')
+            
+        
+            search_results_ids = search_Article(query)
+            
+            
+            articles = Article.objects.filter(id__in=search_results_ids)
+            
+            
+            serializer = Article_results(articles, many=True)
+            serialized_data = serializer.data
+            json_data = json.dumps(serialized_data)
+
+            #print(json_data)
+            #return Response({'results': serialized_data}, status=status.HTTP_200_OK)
+            return Response(json_data, status=status.HTTP_200_OK)
+
+           
+
+
 
                 
             
