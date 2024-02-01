40f7a8c5304bd86967599b743c01ac2cf4d84112
Amine1820 <la_melzi@esi.dz>
2024-02-01 15:11:16 +0100

    Revert "upload all the pdfs in dropbox"
    This reverts commit 2a04c9f0528dddc6ac302c59eebaa036e2cbe176.

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -43,6 +43,26 @@ class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
 
     
 
+def upload(request):
+
+        ACCESS_TOKEN = "sl.BtP1DeW7grVaf7D8n3T7EjNvCzbmlI_5TACBriYkK15nnKBcn2MacPcDCTxQP1U1l0jS3gketxvH3GUOmRWcdW57zY88bCrbhVpH7jyspbPjMlnevKJp6VoCJeaSlGEQZP6OdcVk_5qo4ZQa-HgAm0c"
+        dbx = dropbox.Dropbox(ACCESS_TOKEN)
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(current_directory ,'..',  'drive-download-20231228T162223Z-001', f'Article_03.pdf')
+
+        with open(path, 'rb') as f:
+            dbx.files_upload(f.read(), '/PDF1.pdf')
+
+        
+    
+        # current_directory = os.path.dirname(os.path.abspath(__file__))
+        # path = os.path.join(current_directory ,'..',  'drive-download-20231228T162223Z-001', f'Article_03.pdf')
+        # Response = extractpdf(path)
+    
+        return JsonResponse({"key" : "True"})
+
+
+
 def extract(request):
   
         current_directory = os.path.dirname(os.path.abspath(__file__))
